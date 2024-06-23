eb7e419d0c5e4b937c67fd65cd9fc690c6dc6a8d
GSRK-BZA <gsrkbza@gmail.com>
2024-06-23 10:12:48 +0530

    Made it reload uncontrolablly

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
 import { legacy_createStore } from "redux";
 function cartReducer(state={items : {}}, action){
-
+    switch(action.type){
+        case "cart/add":{
+            break;
+        }
+        case "cart/remove":{
+            break;
+        }
+        default:{
+            return state;
+            break;
+        } 
+    }
 }
 const store = legacy_createStore(cartReducer);
 export default store;
