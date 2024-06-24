dbfc8bb81fe3e3235b0983d9e99ca22dc535f7ca
GSRK-BZA <gsrkbza@gmail.com>
2024-06-24 10:23:53 +0530

    Stopped uncontrollable realoading

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
